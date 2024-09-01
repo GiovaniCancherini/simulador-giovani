0b4ebdd2841c821a0d56ad2a9811527fe4d672ae
Giovani Cancherini <giovani.canquerini@gmail.com>
2024-09-01 19:30:44 -0300

    feat: find arrivals

--- simulador.py
+++ simulador.py
@@ -25,13 +25,13 @@ while (count > 0) {
 	}
 }
 """
-num_events = 100000
+num_events = 100
 arrival_interval = (2.0, 5.0)
 service_interval = (3.0, 5.0)
 max_queue_length = 5
 
 
-def simulate_queue():
+def simulate_queue(num_events, arrival_interval, service_interval, max_queue_length):
     global_time = 0
     next_arrival = 2.0  # Primeiro cliente chega no tempo 2.0
     next_departure = 0
@@ -41,9 +41,17 @@ def simulate_queue():
     
     while num_events > 0:
         if next_arrival < next_departure:
-           # Chegada 
+            global_time = next_arrival
+            events.append((global_time, 'arrival'))
+            num_events -= 1
+            print("arrival")
         else:
-            # Atendimento
+            global_time = next_departure
+            events.append((global_time, 'departure'))
+            num_events -= 1
+            print("departure")
     return 0
     
-global_time = simulate_queue()
+global_time = simulate_queue(num_events, arrival_interval, service_interval, max_queue_length)
+
+print("Tempo total de simulação:", global_time)
