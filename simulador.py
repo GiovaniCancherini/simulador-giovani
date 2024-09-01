853fe2a27ea5f5ab1b4d8cc9d708984e5b741447
Giovani Cancherini <giovani.canquerini@gmail.com>
2024-09-01 19:35:41 -0300

    feat: on the way

--- simulador.py
+++ simulador.py
@@ -25,33 +25,36 @@ while (count > 0) {
 	}
 }
 """
-num_events = 100
-arrival_interval = (2.0, 5.0)
-service_interval = (3.0, 5.0)
-max_queue_length = 5
-
-
 def simulate_queue(num_events, arrival_interval, service_interval, max_queue_length):
     global_time = 0
     next_arrival = 2.0  # Primeiro cliente chega no tempo 2.0
-    next_departure = 0
+    next_departure = float('inf')  # Inicialmente, sem clientes para atender
     queue = []
-    events = []
-    accumulated_times = [0] * (max_queue_length + 1)  # Tempos acumulados para cada estado da fila
+    events = []  # Lista para armazenar tempos de chegada e saída
+    lost_customers = 0  # Contador de clientes perdidos
     
     while num_events > 0:
         if next_arrival < next_departure:
             global_time = next_arrival
             events.append((global_time, 'arrival'))
             num_events -= 1
-            print("arrival")
+            
+            # Tratamento do evento de chegada
+            if len(queue) < max_queue_length:
+                #   
+                
+            # Agendar próxima chegada
+        
         else:
-            global_time = next_departure
-            events.append((global_time, 'departure'))
-            num_events -= 1
-            print("departure")
-    return 0
-    
+            # else
+    return global_time
+            
+
+num_events = 10
+arrival_interval = (2.0, 5.0)
+service_interval = (3.0, 5.0)
+max_queue_length = 5
+
 global_time = simulate_queue(num_events, arrival_interval, service_interval, max_queue_length)
 
 print("Tempo total de simulação:", global_time)
